6effea01b514395ac450a451dee5901b65c35225
blueorion05 <jhudielatienza2k20@gmail.com>
2023-12-07 11:15:20 +0800

    Updated some class and methods.

--- src/App.java
+++ src/App.java
@@ -1,49 +1,61 @@
 import java.util.Scanner;
 
 class Intro {
-    void onStart() {
-        Functions functions = new Functions();
-
-        while(true){
+    void Home() {
+        System.out.println("POSsy - A Point-Of-Sales Console System");
+        System.out.println("By Al Hans Gaming");
+        System.out.println("\nSelect Type of Transaction: ");
+        System.out.println("1 - New Transaction");
+        System.out.println("2 - Previous Transactions");
+        System.out.println("3 - Inventory");
+        System.out.println("4 - Exit");
+        
+        Selection();
+    }
+    
+    void Selection() {
+        try {
             Scanner scn = new Scanner(System.in);
-            System.out.println("POSsy - A Point-Of-Sales Console System");
-            System.out.println("By Al Hans Gaming");
-            System.out.println("\n Select Type of Transaction: ");
-            System.out.println("1 - New Transaction");
-            System.out.println("2 -  Review Previous Transactions");
-
             System.out.print("Transaction: ");
             int choice = scn.nextInt();
 
-            switch (choice)
-            {
+            switch (choice) {
                 case 1:
-                    functions.clearConsole();
-                    functions.Function1();
+                    Functions.clearConsole();
+                    NewTransaction startNewTransac = new NewTransaction();
+                    startNewTransac.StartNewTransac();
                     break;
                 case 2:
-                    functions.clearConsole();
-                    functions.Function2();
+                    Functions.clearConsole();
+                    PreviousTransaction startPreviousTransaction = new PreviousTransaction();
+                    startPreviousTransaction.StartPreviousTransaction();
+                    break;
+                case 3:
+                    Functions.clearConsole();
+                    Inventory startInventory = new Inventory();
+                    startInventory.StartInventory();
+                    break;
+                case 4:
+                    Functions.clearConsole();
+                    System.out.println("Thank you for checking us out.\n");
                     break;
                 default:
-                    System.out.println("Invalid type of transaction, please choose from the choices, wag na maghanap ng wala");
+                    Functions.clearConsole();
+                    System.out.println("Invalid type of transaction.\n");
+                    Home();
+                    break;
             }
         }
+        catch (Exception ex) {
+            Functions.clearConsole();
+            System.out.println("Invalid type of transaction.\n");
+            Home();
+        }  
     }
-
-    
 }
 
-class Functions{
-    void Function1() {
-        System.out.println("FUNCTION uno");
-    }
-
-    void Function2() {
-        System.out.println("FUNCTION dos");
-    }
-
-    void clearConsole() {
+class Functions {
+    static void clearConsole() {
         final String os = System.getProperty("os.name");
 
         try {
@@ -54,17 +66,115 @@ class Functions{
             System.out.println(e);
         }
     }
-    
 }
 
+class NewTransaction {
+    void StartNewTransac() {
+        System.out.println("New Transaction");
+        System.out.println("4 - Back");
 
+        Selection();
+    }
+
+    void Selection() {
+        try {
+            Scanner scn = new Scanner(System.in);
+            int choice = scn.nextInt();
+
+            switch (choice) {
+                case 4:
+                    Functions.clearConsole();
+                    Intro back = new Intro();
+                    back.Home();
+                    break;
+                default:
+                    Functions.clearConsole();
+                    System.out.println("Invalid selection.\n");
+                    StartNewTransac();
+                    break;
+            }
+        }
+        catch (Exception ex) {
+            Functions.clearConsole();
+            System.out.println("Invalid selection.\n");
+            StartNewTransac();
+        }
+    }
+}
+
+class PreviousTransaction {
+    void StartPreviousTransaction() {
+        System.out.println("Previous Transaction");
+        System.out.println("4 - Back");
+
+        Selection();
+    }
+
+    void Selection() {
+        try {
+            Scanner scn = new Scanner(System.in);
+            int choice = scn.nextInt();
+
+            switch (choice) {
+                case 4:
+                    Functions.clearConsole();
+                    Intro back = new Intro();
+                    back.Home();
+                    break;
+                default:
+                    Functions.clearConsole();
+                    System.out.println("Invalid selection.\n");
+                    StartPreviousTransaction();
+                    break;
+            }
+        }
+        catch (Exception ex) {
+            Functions.clearConsole();
+            System.out.println("Invalid selection.\n");
+            StartPreviousTransaction();
+        }
+    }
+}
+
+class Inventory {
+    void StartInventory() {
+        System.out.println("Inventory");
+        System.out.println("4 - Back");
+
+        Selection();
+    }
+
+    void Selection() {
+        try {
+            Scanner scn = new Scanner(System.in);
+            int choice = scn.nextInt();
+
+            switch (choice) {
+                case 4:
+                    Functions.clearConsole();
+                    Intro back = new Intro();
+                    back.Home();
+                    break;
+                default:
+                    Functions.clearConsole();
+                    System.out.println("Invalid selection.\n");
+                    StartInventory();
+                    break;
+            }
+        }
+        catch (Exception ex) {
+            Functions.clearConsole();
+            System.out.println("Invalid selection.\n");
+            StartInventory();
+        }
+    }
+}
 
 public class App {
     public static void main(String[] args) {
         Intro intro = new Intro();
-        Functions functions = new Functions();
 
-        functions.clearConsole();
-        intro.onStart();
+        Functions.clearConsole();
+        intro.Home();
     }
 }
