ec63a2ca6a390d7d96839f85f120bf98fbb10671
Jhondale Llaguno <jhondalellaguno@gmail.com>
2023-12-09 09:37:05 +0800

    Unstage commit.

--- src/Inventory.java
+++ src/Inventory.java
@@ -20,6 +20,7 @@ public class Inventory {
                 System.out.println("[10] Headset");
                 System.out.println("[11] Microphone");
                 System.out.println("[12] Accessory");
+                System.out.println("[13] Search");
                 System.out.println("[0] Back");
                 System.out.print("\nClassification: ");
 
@@ -29,33 +30,61 @@ public class Inventory {
                     Functions.clearConsole();
                     return;
                 }
-                else if (classification > 0 & classification < 13) {
+                else if (classification == 13) {
+                    try {
+                        Functions.clearConsole();
+                        DatabaseHelper databaseHelper = new DatabaseHelper();
+                        Item item = new Item();
+
+                        System.out.print("Item ID: ");
+                        item = databaseHelper.getItem(Functions.getChoice());
+                        Functions.clearConsole();
+                        displayItem(item);
+
+                        System.out.print("Press Enter to go back...");
+                        Functions.getChoiceInString();
+                        Functions.clearConsole();
+                        
+                    } catch (Exception e) {
+                        Functions.clearConsole();
+                        System.out.println("Invalid Item ID!");
+                    }
+                }
+                else if (classification > 0 & classification < 14) {
                     Functions.clearConsole();
+                    // Store and display the retrieved items
+                    ArrayList<Item> items = new ArrayList<>();
+                    DatabaseHelper databaseHelper = new DatabaseHelper();
+
+                    items = databaseHelper.getItems(classification);
+                    displayItemsByCategory(items);
+
+                    System.out.print("Press Enter to go back...");
+                    Functions.getChoiceInString();
+                    Functions.clearConsole();
+
                     break;
                 }
-                
-                Functions.clearConsole();
-                System.out.println("Invalid Classification\n");
+                else {
+                    Functions.clearConsole();
+                    System.out.println("Invalid Classification\n");
+                }
             }
-
-            // Store and display the retrieved items
-            ArrayList<Item> items = new ArrayList<>();
-            DatabaseHelper databaseHelper = new DatabaseHelper();
-
-            items = databaseHelper.getItems(classification);
-            displayItemsByCategory(items);
-
-            System.out.println("\n=================================");
-            System.out.print("Press Enter to go back...");
-            Functions.getChoiceInString();
-            Functions.clearConsole();
         }
+    }
+
+    public void displayItem(Item item) {
+        System.out.println("[" + item.getItemPk() + "] " + item.getProduct());
+        System.out.println("Price: Php." + String.format("%,.2f", item.getPrice()));
+        System.out.println("Stock: " + item.getStock() + "pc");
+        System.out.println("______________________________________________________");
     }
 
     public void displayItemsByCategory(ArrayList<Item> items) {
         for (Item item : items) {
             System.out.println("[" + item.getItemPk() + "] " + item.getProduct());
-            System.out.println("Php." + item.getPrice() + " -> " + item.getStock() + "x");
+            System.out.println("Price: Php." + String.format("%,.2f", item.getPrice()));
+            System.out.println("Stock: " + item.getStock() + "pc");
             System.out.println("______________________________________________________");
         }
     }
